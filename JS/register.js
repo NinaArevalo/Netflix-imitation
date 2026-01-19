78deb5ebad8e8463f262aaea00b46a9a24191ccd
NinaArevalo <andreina.arevalopidiache@gmail.com>
2026-01-19 05:49:36 -0500

    adding firs register part with JS folders, default user, and array to new users

---
+++ JS/register.js
@@ -0,0 +1,11 @@
+//first we need to create a default user
+const defaultUser = {
+    email: "user@email.com",
+    password: "1234",
+    profiles: [
+        {id:1, names: "Main" }
+    ],
+    favorites: {
+        1:[]
+    }
+} 
---
+++ JS/storage.js
@@ -0,0 +1,9 @@
+const USERS_KEY = "users";
+
+function getUsers() {
+    return JSON.parse (localStorage.getItem(USERS_KEY)) || [];
+} // "give me a list with save users, if there aren't anything get me an array empty”
+
+function saveUsers () {
+    localStorage.setItem(USERS_KEY, JSON.stringify(users));
+} // save the users array in the localstorage
